c1943e3763cccd8fdd518d78afe1c34a2b392880
Alex <Alejandro Rodriguez>
2015-05-14 12:30:58 -0500

    corrigiendo grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,14 @@ module.exports=function(grunt){
    	 pkg: grunt.file.readJSON('package.json'),
     concat: {
       all: {
-        src: "./public/assets/js/*/*.js",
-		dest:"./public/assets/js/todo.js"
+        src: ["./public/assets/js/vendor/jquery.js","./public/assets/js/vendor/json2.js","./public/assets/js/vendor/underscore.js","./public/assets/js/vendor/handlebars.js","./public/assets/js/vendor/backbone.js","./public/assets/js/vendor/backbone.marionette.js","./public/assets/js/app.js","./public/assets/js/entities/collec.js","./public/assets/js/entities/controller.js","./public/assets/js/entities/initView.js","./public/assets/js/entities/vistaTemplate.js"],
+		dest:"./public/assets/js/all.js"
        }}
      ,
     uglify: {
       all: {
-        src: "./public/assets/js/todo.js",
-        dest: './public/assets/js/todo.min.js'
+        src: "./public/assets/js/all.js",
+        dest: './public/assets/js/all.min.js'
       }}
 
   });
